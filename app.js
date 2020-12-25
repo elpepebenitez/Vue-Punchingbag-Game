8d815249f61ee2a4544fa73f22229978453d1526
Jose Benitez <pepebenitez12@gmail.com>
2020-12-25 09:08:12 -0500

    Pushing basic files

---
+++ app.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+
+    },
+    methods: {
+        /*addToA: function(){
+            console.log('I ran');
+            return this.age + this.a;
+        },*/
+    },
+    computed: {
+
+    },
+});
